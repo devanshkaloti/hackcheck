5c964fe84e32629f12bc039fa68fc5c704e98049
Devansh Kaloti <devansh@dksources.com>
2023-09-19 00:20:33 -0400

    Update ViewController.swift

--- Hack Check/ViewController.swift
+++ Hack Check/ViewController.swift
@@ -13,13 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        
-//        get(url: "https://haveibeenpwned.com/api/v3/breachedaccount/amandeep%40sunrisemetals.com") { (data) in
-//            let rows = Email.decode(data)
-//            print(rows)
-//        }
-//https://haveibeenpwned.com/api/v3/breachedaccount/amandeep%40sunrisemetals.com
+    
         get(url: "https://haveibeenpwned.com/api/v3/breach/Adobe") { (data) in
             let rows = Site.decode(data)
             print(rows)
